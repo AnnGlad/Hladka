0264b3b39cc19ee261c841eba20da380f82be873
Anna Hladka <namikosamahime@gmail.com>
2020-02-27 23:02:17 +0200

    start page without functionality

--- js/main.js
+++ js/main.js
@@ -1,55 +1,65 @@
-let doubleSlider = document.querySelector(".double-slider");
-let sliderTrack = document.querySelector("#slider-1 .slider-track");
-// let sliderTrack = document.querySelector(".slider-track");
-
-
-let sliderItemsAll = sliderTrack.querySelectorAll(".slide").length;
-let sliderItem = sliderTrack.querySelector(".slide");
-let slideHeight = sliderItem.offsetHeight;
-
-sliderTrack.style.height = slideHeight * sliderItemsAll;
-let slider = document.querySelector("#slider-1");
-let sliderList = document.querySelector("#slider-1 .slider-list");
-// let slider = document.querySelector(".slider");
-// let sliderList = document.querySelector(".slider-list");
-
-sliderList.style.height = slideHeight + "px";
-console.log(sliderTrack);
-console.log(sliderItem);
-console.log(slideHeight);
-console.log(sliderItemsAll);
-
-
-let counterUp = 0;
-let counterDown = 0;
-doubleSlider.onclick = function (event) {
-  let target = event.target;
-  // console.log(target);
-  if (target == this.querySelector(".slider-btn__up")) {
-
-
-    // if (counterUp == 0) {
-    //   sliderTrack.style.transform = "translate3d(0px, -" + ((slideHeight * (sliderItemsAll - 1)) + 20) + "px, 0px)";
-    //   counterUp++;
-    // } else if (sliderItemsAll > counterUp) {
-    //   sliderTrack.style.transform = "translate3d(0px, -" + ((slideHeight * counterUp) + 20) + "px, 0px)";
-
-    // }
-    console.log("up");
+function slider(id) {
+  let slider = document.getElementById(id);
+  let sliderList = slider.querySelector(".slider-list");
+  let sliderTrack = slider.querySelector(".slider-track");
+  let sliderItems = sliderTrack.getElementsByClassName("slide");
+  let sliderItemsAll = sliderItems.length;
+  let itemHeights = []
+  for (let i = 0; i < sliderItemsAll; i++) {
+    itemHeights.push(sliderItems[i].offsetHeight);
   }
+  let maxItemHeight = Math.max.apply(null, itemHeights);
+  for (let i = 0; i < sliderItemsAll; i++) {
+    sliderItems[i].style.height = maxItemHeight + "px";
+  }
+  let sliderItem = sliderTrack.querySelector(".slide");
+  let sliderItemHeight = sliderItem.offsetHeight;
+  sliderTrack.style.height = sliderItemHeight * sliderItemsAll;
+  sliderList.style.height = sliderItemHeight + "px";
 
-  if (target == this.querySelector(".slider-btn__down")) {
-    counterDown++;
-    if (sliderItemsAll > counterDown) {
-      sliderTrack.style.transform = "translate3d(0px, -" + ((slideHeight * counterDown) + 20) + "px, 0px)";
-    } else {
-      sliderTrack.style.transform = "translate3d(0px, 0px, 0px)";
-      counterDown = 0;
+
+  let counterUp = 0;
+  let counterDown = 0;
+
+  slider.onclick = function (event) {
+    let target = event.target;
+    if (target == this.querySelector(".slider-btn__up")) {
+      counterUp++
+      // if (counterUp <= sliderItemsAll && counterUp != 1) {
+      //   console.log("to previous");
+      //   sliderTrack.style.transform = "translate3d(0px, -" + (sliderItemHeight * counterUp) + "px, 0px)";
+
+
+      // } else {
+      //   console.log("to last");
+      //   sliderTrack.style.transform = "translate3d(0px, -" + (sliderItemHeight * (sliderItemsAll - 1)) + "px, 0px)";
+      //   counterUp = 1;
+      // }
+
+      console.log("up");
     }
 
+    if (target == this.querySelector(".slider-btn__down")) {
+      counterDown++;
+      if (sliderItemsAll > counterDown) {
+        sliderTrack.style.transform = "translate3d(0px, -" + (sliderItemHeight * counterDown) + "px, 0px)";
+        sliderItems[(counterDown - 1)].classList.remove("current");
+        console.log("to next");
+      } else {
+        sliderTrack.style.transform = "translate3d(0px, 0px, 0px)";
+        sliderItems[(counterDown - 1)].classList.remove("current");
+        counterDown = 0;
+        console.log("to first");
+      }
+      sliderItems[counterDown].classList.add("current");
+    }
     console.log("down");
   }
 
 }
 
+window.onload = function () {
+  slider("slider-1");
+  slider("slider-2");
 
+}
